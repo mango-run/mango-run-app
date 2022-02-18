d623d2161550c033393e2f880dd55bbfb32e0843
callmewhy <whywanghai@gmail.com>
2022-02-19 01:17:01 +0800

    init electron with react

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: 'erb',
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    createDefaultProgram: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {},
+      webpack: {
+        config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
+      },
+      typescript: {},
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+}
